d71e1b9d97479bf16e23125dbd776d8fb1bb1ea4
Richa <richa008@gmail.com>
2019-12-29 18:37:56 -0800

    Implementing tic tac toe game

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,12 +2,61 @@ import React from 'react';
 import Square from "./Square";
 
 export default class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+
+    calculateWinner() {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        const squares = this.state.squares;
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    handleSquareClick(i) {
+        const squares = this.state.squares.slice();
+        if (squares[i] || this.calculateWinner()) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+
     renderSquare(i) {
-        return <Square />;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleSquareClick(i)}/>;
     }
 
     render() {
-        const status = "Next Player: X";
+        const winner = this.calculateWinner();
+        let status = "";
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player: ${this.state.xIsNext ? "X" : "0" }`;
+        }
 
         return (
             <div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default class Square extends React.Component {
-    
-    render() {
-        return (
-            <button className="square"></button>
-        )
-    }
+export default function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick} >
+            {props.value}
+        </button>
+    )
 }
